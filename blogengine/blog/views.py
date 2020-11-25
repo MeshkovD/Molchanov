b810025c5bafdda7eb5fd1f3337bead9273abe94
“Мешков <rdm.v@mail.ru>
2020-11-25 11:24:25 +0700

    Донастроил форму для создания Tag. Сделал проверку уникальности и запрещенности тэга.

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import View
 from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
 
 from .models import Post, Tag
 from .utils import ObjectDetailMixin
+from .forms import TagForm
+
+
+
+
 def posts_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/index.html', context={'posts': posts})
@@ -22,3 +28,16 @@ class TagDetail(ObjectDetailMixin, View):
 def tags_list(request):
     tags = Tag.objects.all()
     return render(request, 'blog/tags_list.html', context={'tags':tags})
+
+class TagCreate(View):
+    def get(self, request):
+        form = TagForm()
+        return render(request, 'blog/tag_create.html', context={'form': form})
+
+    def post(self, request):
+        bound_form = TagForm(request.POST)
+
+        if bound_form.is_valid():
+            new_tag = bound_form.save()
+            return redirect(new_tag)
+        return render(request, 'blog/tag_create.html', context={'form': bound_form})
