fc87a2003615490828ef3e0d4e99f98373e4ed31
“Мешков <rdm.v@mail.ru>
2020-11-26 10:48:15 +0700

    Сделал миксин для контроллеров создания поста и тега

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
 
 from .models import Post, Tag
-from .utils import ObjectDetailMixin
+from .utils import ObjectDetailMixin, ObjectCreateMixin
 from .forms import TagForm, PostForm
 
 
@@ -29,28 +29,11 @@ def tags_list(request):
     tags = Tag.objects.all()
     return render(request, 'blog/tags_list.html', context={'tags':tags})
 
-class TagCreate(View):
-    def get(self, request):
-        form = TagForm()
-        return render(request, 'blog/tag_create.html', context={'form': form})
+class TagCreate(ObjectCreateMixin, View):
+    model_form = TagForm
+    template = 'blog/tag_create.html'
 
-    def post(self, request):
-        bound_form = TagForm(request.POST)
 
-        if bound_form.is_valid():
-            new_tag = bound_form.save()
-            return redirect(new_tag)
-        return render(request, 'blog/tag_create.html', context={'form': bound_form})
-
-class PostCreate(View):
-    def get(self, request):
-        form = PostForm()
-        return render(request, 'blog/post_create.html', context={'form': form})
-
-    def post(self, request):
-        bound_form = PostForm(request.POST)
-
-        if bound_form.is_valid():
-            new_post = bound_form.save()
-            return redirect(new_post)
-        return render(request, 'blog/post_create.html', context={'form': bound_form})
+class PostCreate(ObjectCreateMixin, View):
+    model_form   = PostForm
+    template = 'blog/post_create.html'
