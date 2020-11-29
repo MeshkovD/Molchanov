02865f38b661f510652afd7ae49cd5e6c4d77b7e
“Мешков <rdm.v@mail.ru>
2020-11-29 19:32:17 +0700

    Добавил просто поиск по заголовками и тексту постов.

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 from .models import Post, Tag
 from .utils import ObjectDetailMixin, ObjectCreateMixin, ObjectUpdateMixin, ObjectDeleteMixin
@@ -11,8 +12,16 @@ from .forms import TagForm, PostForm
 
 
 def posts_list(request):
-    posts = Post.objects.all()
-    paginator = Paginator(posts, 1)
+    search_query = request.GET.get('search', '')
+
+    if search_query:
+        posts = Post.objects.filter(Q(title__icontains=search_query) | Q(body__icontains=search_query))
+    else:
+        posts = Post.objects.all()
+
+
+    # posts = Post.objects.all()
+    paginator = Paginator(posts, 10)
     page_number = request.GET.get('page', 1)
     page = paginator.get_page(page_number)
 
